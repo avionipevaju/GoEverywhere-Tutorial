e681ddadb1ad38879c4f41f493c5c86b16ef9fb3
avionipevaju <antic.nikola@outlook.com>
2017-12-03 13:37:21 +0100

    Added introduction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+const appRoutes: Routes = [
+  {path: '', component: IntroductionComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    IntroductionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
