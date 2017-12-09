45bc19b2b4c321182f529cff49b6125084d1451f
avionipevaju <antic.nikola@outlook.com>
2017-12-09 21:56:23 +0100

    Added next lesson button

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LoaderService} from './loader.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-example',
@@ -13,9 +13,11 @@ export class ExampleComponent implements OnInit {
   title;
   text;
   subtitle;
+  nextLesson;
+  nextStage;
   imageObjects: any[];
 
-  constructor(private route: ActivatedRoute, private loaderService: LoaderService) { }
+  constructor(private route: ActivatedRoute, private loaderService: LoaderService, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(param => {
@@ -29,12 +31,20 @@ export class ExampleComponent implements OnInit {
     this.title = this.content['Title'];
     this.text = this.content['Text'];
     this.subtitle = this.content['Subtitle'];
+    this.nextLesson = this.content['NextLesson'];
+    this.nextStage = this.content['NextStage'];
     this.imageObjects = [];
     for (const x in this.content) {
       if (Number.isInteger(Number(x))) {
         this.imageObjects.push({'url': this.content[x]['ImageUrl'], 'description': this.content[x]['ImageDescription'] });
       }
     }
+  }
+
+  next() {
+    console.log(this.nextLesson);
+    console.log(this.nextStage);
+    this.router.navigate([this.nextLesson, {level: this.nextStage}] );
   }
 
 }
